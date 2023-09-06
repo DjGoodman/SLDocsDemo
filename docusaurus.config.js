79aa5ff36a767eeb2ca5f11fa9f3f094a526ca03
DjGoodman <Daniel.Goodman@remarkable.net>
2023-09-06 18:01:42 +0100

    config changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,19 +7,20 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Squash is cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/SLDocsDemo/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'DjGoodman', // Usually your GitHub org/user name.
+  projectName: 'SLDocsDemo', // Usually your repo name.
+  deploymentBranch: "gh-pages"
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
